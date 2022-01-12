aaf33f3af877bc4c360e619787c2aa78c316c2c8
Ileenfdz <ileenfdz@gmail.com>
2022-01-12 23:14:59 +0100

    Added variables

---
+++ variables.php
@@ -0,0 +1,13 @@
+<?php
+//In php there's no need to separate types, like in js
+
+//string
+    $myName = 'Daniel';
+//integer
+    $myAge = 28;
+//boolean
+    $havePet = true;
+
+//To make a constant variable, always put it in Upper Case
+    CONST PI = 3.14;
+    CONST LIKINGS = true;
