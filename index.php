87f48af4ec57de7a6f41702b459356b3a317ee1b
Ileenfdz <ileenfdz@gmail.com>
2022-01-12 22:47:24 +0100

    Added examples of what is and what not, scaping

--- index.php
+++ index.php
@@ -7,6 +7,31 @@
     <title>First exercise</title>
 </head>
 <body>
-    
+
+    <p>This is a paragraph</p>
+
+    <?php 
+        echo "Hello World";//Show on screen
+
+        //You can also point out tags inside the echo, and be like regular html
+        echo "<p>Hello World</p>";
+
+        print "Hello php";//Show on screen
+    ?>
+
+    <!-- Without scaping -->
+    <?php
+        if (1 == 1) {
+            echo("<p>This works! Yay!</p>");
+        }
+    ?>
+
+    <!-- With escaping -->
+    <?php if (1 == 1) { ?>
+        <p>This works! Yay!</p>
+    <?php } ?>
+    <!--Best to keep them separated using scaping -->
+
+    <p>This is another paragraph</p>
 </body>
 </html>
