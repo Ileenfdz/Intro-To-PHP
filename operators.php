cec069e44c0ce141536f342d72357673b41fdb57
Ileenfdz <ileenfdz@gmail.com>
2022-01-12 23:34:41 +0100

    Added operators

---
+++ operators.php
@@ -0,0 +1,67 @@
+<?php
+
+/*Types:
+-Operator Predecede
+-Arithmetic Operators
+-Assignment Operators
+-Bitwise Operators
+-Comparison Operators
+-Error Control Operators
+-Execurion Operators
+-Incrementing/Decrementing Operators
+-Logical Operators
+-String Operators
+-Array Operators
+-Type Operators
+*/
+
+/*Arithmetic Operators:
+    $sum = $num1 + num2;
+    $sum = $num1 - num2;
+    $sum = $num1 / num2;
+    $sum = $num1 * num2;
+*/
+
+/*Assigment Operators:
+    $num1 += num2;
+    This one above, is a shorthand for this
+    $num1 = $num1 + $num2;
+*/ 
+
+/*Comparison Operators:
+    $num1 == $num2;
+    $num1 === $num2;
+    $num1 != $num2;
+    $num1 !== $num2;
+    $num1 < $num2;
+    $num1 <= $num2;
+    $num1 > $num2;
+    $num1 >= $num2;
+    $num1 <=> $num2;
+*/
+
+/*Logical Operators:
+    $num1 = 5;
+    $num2 = 10;
+
+    $num1 === 5 and $num2 === 10;
+    Shorthand
+    $num1 === 5 && $num2 === 10;
+
+    $num1 === 5 or $num2 === 10;
+    Shorthand
+    $num1 === 5 || $num2 === 10;
+
+    $num1 === 5 xor $num2 === 10; Only one has to be true, just one
+
+    !$num1 === 6; Is it false?
+*/
+
+/*Increment/Decrement:
+    $num1 = 5;
+    ++$num1; Increments, output 6
+    $num1++; Nothing, outputs 5
+
+    --$num1; Increments, output 4
+    $num1--; Nothing, outputs 5
+*/
